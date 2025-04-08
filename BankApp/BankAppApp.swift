1f4995ca316f67eb2b06e67bd8aeca08021cee7a
vhenriquebm <vhenriquebm@gmail.com>
2025-04-08 19:17:35 -0300

    Initial Commit

---
+++ BankApp/BankAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankAppApp.swift
+//  BankApp
+//
+//  Created by Vitor Henrique Barreiro Marinho on 08/04/25.
+//
+
+import SwiftUI
+
+@main
+struct BankAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
