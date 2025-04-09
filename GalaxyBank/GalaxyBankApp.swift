9600041d74f82cc93aa1c069c2e217d9070df609
vhenriquebm <vhenriquebm@gmail.com>
2025-04-09 16:18:03 -0300

    sceme configuration

---
+++ GalaxyBank/GalaxyBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalaxyBankApp.swift
+//  GalaxyBank
+//
+//  Created by Vitor Henrique Barreiro Marinho on 09/04/25.
+//
+
+import SwiftUI
+
+@main
+struct GalaxyBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GalaxyBankTests/GalaxyBankTests.swift
@@ -0,0 +1,17 @@
+//
+//  GalaxyBankTests.swift
+//  GalaxyBankTests
+//
+//  Created by Vitor Henrique Barreiro Marinho on 09/04/25.
+//
+
+import Testing
+@testable import GalaxyBank
+
+struct GalaxyBankTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
